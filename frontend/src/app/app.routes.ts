9b5f0f1030721853cfde7cb3458888d2bdd11215
Ted Katseres <233443+tedkat@users.noreply.github.com>
2026-03-22 20:37:14 -0500

    clean app.html && ng g component home -s --skip-tests --flat && add home route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: Home },
+];
---
+++ frontend/src/app/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styles: ``,
+})
+export class Home {}
